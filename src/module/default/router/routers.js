29a24cc9e09d926c4c4d88df89aefff45f2e8ec4
李晶 <lijing@bingosoft.net>
2017-12-19 10:26:27 +0800

    模块路由文件合并成一个

--- src/module/default/router/routers.js
+++ src/module/default/router/routers.js
@@ -1,4 +1,5 @@
 var utils=require("libs/router_utils");
-var r=require("./module_require").r;
-var routersData = utils.getModuleRoutes("default",r);
+var routersData = utils.getModuleRoutes("default",function(component){
+    return require('src/module/default/view/' + component);
+});
 module.exports = routersData;
