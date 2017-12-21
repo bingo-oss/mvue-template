c2ad7dba17fa1350750f89428433a0ad56624d3e
fulsh <fulsh@bingosoft.net>
2017-12-21 14:06:00 +0800

    增加操作

--- src/libs/metadata/metagrid_operation.js
+++ src/libs/metadata/metagrid_operation.js
@@ -2,6 +2,25 @@
  * 提供grid内置的操作
  */
 
+/**
+ * 创建操作，跳转到新建表单页
+ * @param context
+ */
+function createOperation(context){
+  var metaEntity=context.metaEntity;
+  var operation= {
+    title:"新建",
+    icon:"plus-round",
+    onclick:function(){
+      alert("not implement");
+    }
+  };
+  return operation;
+}
+
+
+
+
 export default {
 
 }
