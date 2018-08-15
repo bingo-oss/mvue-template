677c65f9d6273aa10b7549e4a096022da08285e8
李晶 <lijing@bingosoft.net>
2018-08-15 17:03:25 +0800

    修改configUrl

--- static/config/config.js
+++ static/config/config.js
@@ -1,5 +1,4 @@
 window.config={
   //配置服务地址
-	configUrl:"https://developer.bingosoft.net:12100/bcf_base/config/published",
-  apiBaseUrl:"https://developer.bingosoft.net:12100/bcf_base"
+	configUrl:"https://developer.bingosoft.net:12000/mvue_engine/config/published"
 }
