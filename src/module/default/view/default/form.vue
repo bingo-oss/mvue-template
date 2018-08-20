08f40a17cc3026dd405a7ee507dcbaaf578fffd9
李晶 <lijing@bingosoft.net>
2018-08-20 10:45:06 +0800

    默认表单标题头文字修正

--- src/module/default/view/default/form.vue
+++ src/module/default/view/default/form.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="bvue-page" :key="$route.fullPath"  :id="'default-form-uuid-'+entityName">
-    <b-childheader :title="'实体管理'" :subtitle="'对实体数据进行编辑'"></b-childheader>
+    <b-childheader :title="childheader.title" :subtitle="childheader.subtitle"></b-childheader>
     <div class="bvue-page-body">
       <Card :bordered="false" :padding="24">
         <Row>
           <Col span="22" >
               <meta-form ref="form" :entity-name="entityName" :record-id="id"
                          :isView="isViewMode" :toolbar="toolbar"
+                         :on-inited="handleOnInited"
                          :label-width="120">
               </meta-form>
           </Col>
@@ -21,6 +22,10 @@ export default {
     var entityName=this.$route.params.entityName;
     var id=this.$route.params.id;
     return {
+      childheader:{
+        title:"",
+        subtitle:""
+      },
       entityName:entityName,
       id:id,
       toolbar:{
@@ -46,6 +51,21 @@ export default {
     isViewMode(){
       let action=this.$route.query&&this.$route.query[Utils.queryKeys.action];
       return Utils.formActions.view===action;
+    }
+  },
+  methods:{
+    handleOnInited(formInst){
+      var metaEntity=formInst.metaEntity;
+      var prefix='';
+      if(this.isViewMode){
+        prefix='查看';
+      }else if(this.id){
+        prefix='编辑';
+      }else{
+        prefix='创建';
+      }
+      this.childheader.title=`${prefix}${metaEntity.title}`;
+      this.childheader.subtitle=`${metaEntity.description}`;
     }
   }
 }
