4a45f0efa891a34266146067e085a661b7e66c45
李晶 <lijing@bingosoft.net>
2018-09-27 11:09:23 +0800

    修正自动页面模板

--- src/templates/index.vue
+++ src/templates/index.vue
@@ -1,16 +1,29 @@
 <template>
-    <meta-layout :settings="settings"></meta-layout>
+  <div class="bvue-page">
+    <b-childheader :title="header.title" :subtitle="header.description" :showBack="header.showBack" ></b-childheader>
+    <div class="bvue-page-body">
+      <Card>
+        <meta-layout :layout="pageSettings.layout" :key="$route.fullPath"></meta-layout>
+      </Card>
+    </div>
+  </div>
 </template>
 <script>
   import confs from '../pages/auto-page-confs';
   import mvueCore from 'mvue-core';
   export default {
-    data: function () {
-        var key=this.$route.fullPath;
-        var _settings=mvueCore.metaLayoutConvertor.convert(confs[key]);
-        return {
-            settings:_settings.layout
-        };
+    data:function(){
+      var key=this.$route.path;
+      var pageSettings=mvueCore.metaLayoutConvertor.convert(confs[key]);
+      var header={
+        title:pageSettings.title||"未设置",
+        description:pageSettings.description
+      };
+      return {
+        pageSettings:pageSettings,
+        header:header
+      };
     }
   };
 </script>
+
