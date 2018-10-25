4322785dd85d776dc8ff39a89b3d16059ba10359
fulsh <fulsh@bingosoft.net>
2018-10-25 14:23:59 +0800

    增加本地配置支持

--- static/config/config.js
+++ static/config/config.js
@@ -1,5 +1,7 @@
 window.config={
   //配置服务地址，可以为远程服务器地址，如 https://developer.bingosoft.net:12100/mvue_engine/config/published
-  //也可以为本服务器配置地址，如 config/config_example.json
-	configUrl:"config/config_example.json"
+  //也可以为本服务器配置地址，如 config/local_config.json
+	//configUrl:"config/local_config.json",
+  //后端API服务器地址
+  apiBaseUrl:""
 }
