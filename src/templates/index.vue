9bdeb1ce35a2256140b70ba88bb70bc43e057141
李晶 <lijing@bingosoft.net>
2018-11-05 10:47:37 +0800

    page组件移到mvue-core

--- src/templates/index.vue
+++ src/templates/index.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="bvue-page">
-    <b-childheader :title="header.title" :subtitle="header.description" :showBack="header.showBack" ></b-childheader>
-    <div class="bvue-page-body">
-      <Card>
-        <meta-layout :layout="pageSettings.layout"></meta-layout>
-      </Card>
-    </div>
-  </div>
+  <meta-page :header="header" :pageSettings="pageSettings"></meta-page>
 </template>
 <script>
   import confs from '../pages/auto-page-confs';
