9ec4461c1b059f1335d75b758a76133011a567b1
李晶 <lijing@bingosoft.net>
2018-11-30 15:42:03 +0800

    系统管理模块的配置管理、权限管理、菜单管理使用mvue-module-system模块库的功能

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import indexBase from 'libs/index_base';
+import mvueToolkit from 'mvue-toolkit';
+
+//应用依赖的模块，通过moduleManager添加进来
+import mvueModuleSystem from 'mvue-module-system';
+mvueToolkit.moduleManager.add(mvueModuleSystem);
 
 //启动应用
 indexBase.startApp();
