f0b6f8a7800b86489fa28e9102f38614c58054ea
李晶 <lijing@bingosoft.net>
2019-02-26 15:22:39 +0800

    去掉一些无用的代码和配置

--- src/libs/index_base.js
+++ src/libs/index_base.js
@@ -80,7 +80,6 @@ function doStart(){
   });
   context.setCurrentVue(vueApp);
   context.getMvueToolkit().moduleManager.initAfterAppStarted(context);
-  let c=Vue.options.components;
 }
 //对外暴露的Vue应用启动函数：先获取应用配置，然后初始化模块，加载元数据信息，最终启动应用
 function startApp() {
