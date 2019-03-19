c65904d0d67c3c145c0832c26c8ee41107e99166
李晶 <lijing@bingosoft.net>
2019-03-19 17:11:10 +0800

    默认模板简化

--- src/templates/index.vue
+++ src/templates/index.vue
@@ -1,23 +1,3 @@
 <template>
-  <meta-page :header="header" :pageSettings="pageSettings"></meta-page>
+    <m-page-tmpl></m-page-tmpl>
 </template>
-<script>
-  import context from '../libs/context';
-  export default {
-    data:function(){
-      var self=this;
-      var key=this.$route.matched[this.$route.matched.length-1].path;
-      var confs=context.getAutoPageConfs();
-      var pageSettings=context.getMvueCore().metaLayoutConvertor.convert(_.cloneDeep(confs[key]),self);
-      var header={
-        title:pageSettings.title,
-        description:pageSettings.description
-      };
-      return {
-        pageSettings:pageSettings,
-        header:header
-      };
-    }
-  };
-</script>
-
